28deb0b92c903b1f0ca40b1733fdc1a22c1ebaf1
gslp999 <gslp999@gmail.com>
2020-02-06 18:47:39 +0530

    Modified services regarding database.

--- CarPooling.Providers/UserService.cs
+++ CarPooling.Providers/UserService.cs
@@ -3,6 +3,7 @@ using CarPooling.Contracts;
 using CarPooling.DataModels;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CarPooling.Providers
@@ -12,12 +13,20 @@ namespace CarPooling.Providers
         public void AddUser(User user)
         {
             Concerns.User _user = user.MapTo<Concerns.User>();
-            DataStore.users.Add(_user);
+            using(var db = new Concerns.CarPoolingDbContext())
+            {
+                db.Add(_user);
+                db.SaveChanges();
+            }
         }
 
         public User GetUser(string email)
         {
-            Concerns.User user = DataStore.users.Find(x => x.Email == email);
+            Concerns.User user;
+            using (var db = new Concerns.CarPoolingDbContext())
+            {
+                user = db.User.Where(x=>x.Email==email).SingleOrDefault();
+            }
             return user.MapTo<User>();
         }
 
